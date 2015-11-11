caf515c699a2a2f5941c0bc94abc2d805c41244b
Frank Zunderer <info@zunderweb.de>
2015-11-11 12:19:09 +0100

    exclude mobile theme 4.7 4.8

--- 4.7-4.8/copy_this/modules/zunderweb/multilevelmenu/components/widgets/multilevelmenu_oxwcategorytree.php
+++ 4.7-4.8/copy_this/modules/zunderweb/multilevelmenu/components/widgets/multilevelmenu_oxwcategorytree.php
@@ -5,9 +5,17 @@ class multilevelmenu_oxwcategorytree extends multilevelmenu_oxwcategorytree_pare
     {
         $this->_sThisTemplate = parent::render();
 
-        if ($this->_sThisTemplate == 'widget/header/categorylist.tpl' ) {
+        if ($this->_sThisTemplate == 'widget/header/categorylist.tpl' && !$this->isThemeMobile() ) {
             $this->_sThisTemplate = 'multilevelmenu_categorylist.tpl';
         }
         return $this->_sThisTemplate;
+    }
+    public function isThemeMobile(){
+        $blIsMobile = false;
+        if ( class_exists('oeThemeSwitcherThemeManager')) {
+            $oThemeManager = new oeThemeSwitcherThemeManager();
+            $blIsMobile = $oThemeManager->isMobileThemeRequested();
+        }
+        return $blIsMobile;
     }
 }
